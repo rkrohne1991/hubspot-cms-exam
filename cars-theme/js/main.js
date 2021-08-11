a0b9cb20df7fcd98f1a25bec23d2beec8ce34494
rkrohne1991 <rafal.krohne@hotmail.co.uk>
2021-08-11 18:57:26 +0100

    Adding more styling, finishing off the header, navigation and hero

---
+++ cars-theme/js/main.js
@@ -0,0 +1,61 @@
+(function() {
+
+    /**
+    * Easy selector helper function
+    */
+    const select = (el, all = false) => {
+        el = el.trim()
+        if (all) {
+            return [...document.querySelectorAll(el)]
+        } else {
+            return document.querySelector(el)
+        }
+    }
+
+    /**
+    * Easy event listener function
+    */
+    const on = (type, el, listener, all = false) => {
+        let selectEl = select(el, all)
+        if (selectEl) {
+            if (all) {
+                selectEl.forEach(e => e.addEventListener(type, listener))
+            } else {
+                selectEl.addEventListener(type, listener)
+            }
+        }
+    }
+
+    /**
+    * Easy on scroll event listener 
+    */
+    const onscroll = (el, listener) => {
+        el.addEventListener('scroll', listener)
+    }
+
+    /**
+    * Mobile nav toggle
+    */
+    on('click', '.mobile-nav-toggle', function(e) {
+        select('#navbar').classList.toggle('navbar-mobile');
+        this.classList.toggle('bi-list');
+        this.classList.toggle('bi-x');
+    });
+
+    /**
+    * Toggle .header-scrolled class to #header when page is scrolled
+    */
+    let selectHeader = select('#header')
+    if (selectHeader) {
+        const headerScrolled = () => {
+        if (window.scrollY > 100) {
+            selectHeader.classList.add('header-scrolled')
+        } else {
+            selectHeader.classList.remove('header-scrolled')
+        }
+        }
+        window.addEventListener('load', headerScrolled)
+        onscroll(document, headerScrolled)
+    }
+
+})();
